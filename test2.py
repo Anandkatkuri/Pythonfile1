976507e49d786b0bea1b967d9866abc996c959f4
Anand Reddy Katkuri <anandkatkuri19@gmail.com>
2022-02-05 20:00:36 -0600

    added test2 to feature

---
+++ test2.py
@@ -0,0 +1,8 @@
+def fun():
+    print("this is the first program")
+    print("this is the second program")
+    print("this is the third program")
+    print("this is the fourth program")
+
+
+fun()
